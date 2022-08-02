64c7688a9427ef188b6b33d2eb232af24c37905a
Sourcery AI <>
2022-08-02 19:11:48 +0000

    'Refactored by Sourcery'

--- python-bare/create_recurring_scans.py
+++ python-bare/create_recurring_scans.py
@@ -8,10 +8,14 @@ if token == "":
     exit(1)
 
 baseURL = "https://console.rumble.run/api/v1.0"
-headers = {"Authorization": "Bearer " + token, "Content-Type": "application/json"}
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json",
+}
+
 
 # Make sure our key is valid
-keyInfo = requests.get(baseURL+"/org/key", data={}, headers=headers).json()
+keyInfo = requests.get(f"{baseURL}/org/key", data={}, headers=headers).json()
 print("API Key: ", keyInfo)
 
 if 'organization_id' not in keyInfo:
@@ -19,20 +23,25 @@ if 'organization_id' not in keyInfo:
     exit(1)
 
 
-agentList = requests.get(baseURL+"/org/agents", data={}, headers=headers).json()
+agentList = requests.get(
+    f"{baseURL}/org/agents", data={}, headers=headers
+).json()
+
 print("Found %d agents" % (len(agentList)))
 
-taskList = requests.get(baseURL+"/org/tasks?status=active", data={}, headers=headers).json()
+taskList = requests.get(
+    f"{baseURL}/org/tasks?status=active", data={}, headers=headers
+).json()
+
 print("Found %d active tasks" % (len(taskList)))
 
-recurringTaskList = []
-for task in taskList:
-    if task["recur"]:
-        recurringTaskList.append(task)
-
+recurringTaskList = [task for task in taskList if task["recur"]]
 print("Found %d recurring tasks" % (len(recurringTaskList)))
 
-siteList = requests.get(baseURL+"/org/sites", data={}, headers=headers).json()
+siteList = requests.get(
+    f"{baseURL}/org/sites", data={}, headers=headers
+).json()
+
 print("Found %d sites" % (len(siteList)))
 
 
@@ -41,10 +50,24 @@ for site in siteList:
     if site["description"] and "api-managed" not in site["description"]:
         # Create a recurring task for this site
         # TODO: Make sure we pick the right agent (local mapping or otherwise)
-        scan = requests.put(baseURL+"/org/sites/"+site["id"]+ "/scan", json={"targets": site["scope"], "rate":"6000", "scan-frequency":"daily"}, headers=headers).json()
-        print("Scan created for site " + site["name"] + ": %s"  % scan)
+        scan = requests.put(
+            f"{baseURL}/org/sites/" + site["id"] + "/scan",
+            json={
+                "targets": site["scope"],
+                "rate": "6000",
+                "scan-frequency": "daily",
+            },
+            headers=headers,
+        ).json()
+
+        print("Scan created for site " + site["name"] + f": {scan}")
 
         # Update the site to indicate we have a task
-        update = requests.patch(baseURL+"/org/sites/"+site["id"], json={"description": "api-managed site"}, headers=headers).json()
-        print("Updated site " + site["name"] + ": %s"  % update)
+        update = requests.patch(
+            f"{baseURL}/org/sites/" + site["id"],
+            json={"description": "api-managed site"},
+            headers=headers,
+        ).json()
+
+        print("Updated site " + site["name"] + f": {update}")
 
